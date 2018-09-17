ad4bf3829602b7d76c58fc12e79aa99b03cb9e7d
WYQ <wyqpadding@gmail.com>
2018-09-17 11:30:54 +0800

    Initial Commit

---
+++ YQThen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQThen
+//
+//  Created by WYQ on 2018/9/17.
+//  Copyright © 2018 WYQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YQThen/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  YQThen
+//
+//  Created by WYQ on 2018/9/17.
+//  Copyright © 2018 WYQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
