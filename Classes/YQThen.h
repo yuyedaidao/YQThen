51e79d70314e77a0fe42dbc74290c2d00bbf5954
WYQ <wyqpadding@gmail.com>
2018-09-17 14:16:02 +0800

    first commit

---
+++ Classes/YQThen.h
@@ -0,0 +1,17 @@
+//
+//  YQThen.h
+//  YQThen
+//
+//  Created by WYQ on 2018/9/17.
+//  Copyright © 2018 WYQ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YQThen : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/YQThen.m
@@ -0,0 +1,13 @@
+//
+//  YQThen.m
+//  YQThen
+//
+//  Created by WYQ on 2018/9/17.
+//  Copyright © 2018 WYQ. All rights reserved.
+//
+
+#import "YQThen.h"
+
+@implementation YQThen
+
+@end
